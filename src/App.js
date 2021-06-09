65174c4a8fadce1910865d296d7cceba1caea937
Stranger <rooderuler@gmail.com>
2021-06-09 13:40:34 +0300

    создание модального окна

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, {useEffect} from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
+
+
+const AddTodo = React.lazy(() => new Promise(resolve => {
+    setTimeout(() => {
+        resolve(import('./Todo/AddTodo'))
+    }, 4000)
+}))
 
 function App() {
     const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
             .then(response => response.json())
             .then(todos => {
-                setTodos(todos)
+                setTimeout(() => {
+                    setTodos(todos)
+                    setLoading(false)
+                }, 2000)
+
             })
     }, [])
 
@@ -41,8 +53,15 @@ function App() {
         <Context.Provider value={{removeTodo: removeTodo}}>
             <div className='wrapper'>
                 <h1>React tutorial</h1>
-                <AddTodo onCreate={addTodo}/>
-                {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>Всё выполнено</p>}
+                <React.Suspense fallback={<p><Loader /></p>}>
+                    <AddTodo onCreate={addTodo}/>
+                </React.Suspense>
+
+                {loading && <Loader/>}
+
+                {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : (
+                    loading ? null : <p>Всё выполнено</p>
+                )}
 
             </div>
         </Context.Provider>
---
+++ src/Loader.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default () => (
+    <div style={{display: 'flex', justifyContent: 'center', margin: '.5Rem'}}>
+        <div className="lds-dual-ring"/>
+    </div>)
